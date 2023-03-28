1912689c902a0ee68a9c56ab439248cfb0f06688
messiasGeovani <messias.geovani.lima@gmail.com>
2023-03-28 19:58:34 -0300

    repositories normalization

--- Domain/Interfaces/IUserRepository.cs
+++ Domain/Interfaces/IUserRepository.cs
@@ -1,7 +1,11 @@
 ﻿using Domain.Entities;
 using Domain.Shared.Interfaces;
+using System.Linq.Expressions;
 
 namespace Domain.Interfaces
 {
-    public interface IUserRepository : IBaseRepository<User> { }
+    public interface IUserRepository : IBaseRepository<User>
+    {
+        Task<List<User>> Search(Expression<Func<User, bool>> query);
+    }
 }
--- Domain/Shared/Interfaces/IBaseRepository.cs
+++ Domain/Shared/Interfaces/IBaseRepository.cs
@@ -1,12 +1,9 @@
-﻿using System.Linq.Expressions;
-
-namespace Domain.Shared.Interfaces
+﻿namespace Domain.Shared.Interfaces
 {
     public interface IBaseRepository<T>
     {
         Task<List<T>> Get();
         ValueTask<T?> GetById(Guid id);
-        Task<List<T>> Search(Expression<Func<T, bool>> query);
         Task Create(T model);
         Task Update(T model);
     }
